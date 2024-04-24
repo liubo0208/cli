b94296de5ca1af13c05201c11a096cd031352990
刘博 <tyliubo0208@gmail.com>
2024-04-24 11:20:53 +0800

    业务代码上传

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use rcli::{CmdExector, Opts};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+    let opts = Opts::parse();
+    opts.cmd.execute().await?;
+
+    Ok(())
 }
